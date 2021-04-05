0874e3fec87f4705a858b6f09ca99f16fb92355a
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-05 16:13:21 -0400

    First commint - app completed

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Grand Circus Room Detail Generator!");
+            string userChoice;
+
+            do
+            {
+                //Getting user's user value
+                Console.WriteLine("Enter Length");
+                double lenVal = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Width");
+                double widthVal = double.Parse(Console.ReadLine());
+
+                //Calculating area and perimeter
+                double pResult = Math.Round(2 * lenVal + 2 * widthVal);
+                double aResult = Math.Round(lenVal * widthVal);
+
+                //Displaying the result to user:
+                Console.WriteLine($"Area: { aResult }\nPerimeter: { pResult }");
+                Console.WriteLine($"Carpet Tiles: {Math.Round(aResult / 5, 3)}");
+                Console.WriteLine($"Paint: {Math.Round(pResult / 5, 3)}");
+
+                //Prompting the user again
+                Console.ReadLine();
+                Console.Clear();
+
+                Console.WriteLine("Would you like to calculate again?: ");
+                userChoice = Console.ReadLine();
+
+            } while (userChoice.ToLower() == "yes");
+        }
+    }
+}
