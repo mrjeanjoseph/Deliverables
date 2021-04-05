cfdad05c565df237a24eb5a3c604032cbc5d34b5
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-05 10:56:49 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of the rectangle");
+            int length = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the width of the rectangle");
+            int width = int.Parse(Console.ReadLine());
+
+            int result = length * width;
+            Console.WriteLine($"The area of the rectangle is: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
