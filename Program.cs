af48fe21658494bc50724280cd8c717941091175
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-05 10:35:44 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("What is your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());         
+
+        Console.WriteLine("What is your favorite color: ");
+        string color = Console.ReadLine();
+
+
+        Console.WriteLine($"Name: " + name);
+        Console.WriteLine($"Age: " + age);
+        Console.WriteLine($"Color: " + color);
+
+        Console.ReadLine();
+    }
+}
