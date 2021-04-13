851e89183c5c78ea8509ad561ddcdaab086a85f6
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-13 08:55:36 -0400

    Final commit

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,65 +7,125 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             Database();
+
             Console.ReadLine();
         }
 
         static void Database()
         {
+            Console.WriteLine("Which student would you like to learn more about?");
+
             string[] nameArray = { "Sean", "Jason", "Kalai", "Jean", "Justin", "Alice", "Kristen", "Kamesha" };
             string[] cityArray = { "Plymouth", "Courtright", "Troy", "Lascahobas", "Wyoming", "Detroit", "Orange Park", "Detroit" };
-            string[] foodArray = { "Pizza", "Dosa", "Fritay", "Baja Blast", "Sushi", "Mac and Cheese", "Macaroni and Cheese", "Tacos" };
+            string[] foodArray = { "Pizza", "Dosa", "Baja Blast", "Fritay", "Sushi", "Mac and Cheese", "Macaroni and Cheese", "Tacos" };
 
-            Console.WriteLine("Which student would you like to learn more about?");
-            int userInput = int.Parse(UserPrompt("Enter anumber 1 - 20"));
+            string userInput = UserPrompt("Enter anumber 1 - 20");
 
-            for (int i = 0; i <= nameArray.Length - 1; i++)
+            bool reRunProgram = true;
+            while (reRunProgram)
             {
-                if (userInput == (i + 1)) // The bug is right here - find it
+                for (int i = 0; i <= nameArray.Length - 1; i++)
                 {
-                    Console.WriteLine($"Student {userInput} is { nameArray[i] }.");
-                    string userInput2 = UserPrompt($"What would you like to know about {nameArray[i]}? \nEnter \"hometown\" or \"favorite food\".");
-                    if (userInput2.ToLower() == "hometown")
+                    if (Convert.ToInt32(userInput) == i + 1 && IsNum(userInput)) // The bug is right here - find it (maybe don't use a forloop
                     {
-                        userInput2 = UserPrompt($"{nameArray[i]} is from {cityArray[i]}. Would you like to know more? (enter “yes” or “no”):");
-                        if (userInput2 == "yes")
+                        Console.WriteLine($"Student {userInput} is { nameArray[i] }.");
+
+                        userInput = UserPrompt($"What would you like to know about {nameArray[i]}? \nEnter \"hometown\" or \"favorite food\".");
+                        if (userInput.ToLower() == "hometown")
                         {
-                            Console.WriteLine($"{nameArray[i]}'s favorite food is {foodArray[i]}.");
+                            userInput = UserPrompt($"{nameArray[i]} is from {cityArray[i]}. Would you like to know more? (enter “yes” or “no”):");
+
+                            if (userInput == "yes")
+                            {
+                                Console.WriteLine($"{nameArray[i]}'s favorite food is {foodArray[i]}.");
+                                break;
+                            }
+                            else if (userInput == "no")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                userInput = UserPrompt("That was not a valid input. (enter “yes” or “no”): ");
+                            }
+
+                        }
+                        else if (userInput.ToLower() == "favorite food")
+                        {
+                            userInput = UserPrompt($"{nameArray[i]}'s favorite food is {foodArray[i]}. Would you like to know more? (enter “yes” or “no”):");
+                            if (userInput == "yes")
+                            {
+                                Console.WriteLine($"{nameArray[i]} is from {cityArray[i]}");
+                                break;
+                            }
+                            else if (userInput == "no")
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                userInput = UserPrompt("That was not a valid input. (enter “yes” or “no”): ");
+                            }
                         }
                         else
                         {
-                            break;
+                            userInput = UserPrompt("That data does not exist. Please try again. (enter “hometown” or “favorite food”)");
                         }
                     }
-                    else if (userInput2.ToLower() == "favorite food")
+                    else if (Convert.ToInt32(userInput) > i + 8)
                     {
-                        userInput2 = UserPrompt($"{nameArray[i]}'s favorite food is {foodArray[i]}. Would you like to know more? (enter “yes” or “no”):");
-                        if (userInput2 == "yes")
-                        {
-                            Console.WriteLine($"{nameArray[i]} is from {cityArray[i]}");
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        userInput = UserPrompt("That student does not exist. Please try again. (enter a number 1-20)");
                     }
                     else
                     {
-                        Console.WriteLine("That data does not exist. Please try again. (enter “hometown” or “favorite food”)");
+                        reRunProgram = false;
                     }
                 }
-                else
+
+                userInput = UserPrompt("Do you want to learn about another student?: y/n");
+                while (true)
                 {
-                    Console.WriteLine("That student does not exist. Please try again. (enter a number 1-20))");
+                    if (userInput == "y")
+                    {
+                        break;
+                    }
+                    else if (userInput == "n")
+                    {
+                        reRunProgram = false;
+                        break;
+                    }
+                    else
+                    {
+                        userInput = UserPrompt("That was not a valid input.");
+                    }
                 }
             }
         }
 
-        static string UserPrompt(string userInput)
+        static bool IsNum(string num)
         {
-            Console.WriteLine(userInput);
-            userInput = Console.ReadLine();
-            return userInput;
+            return int.TryParse(num, out _); // Returns true if is a number
+        }
+
+        static string UserPrompt(string userPrompt)
+        {
+            bool reRun = true; // Rerun the program if empty string
+            Console.WriteLine(userPrompt);
+            string userInput = "";
+            while (reRun)
+            {
+                userInput = Console.ReadLine();
+                if (userInput == "")
+                {
+                    Console.WriteLine($"You did not enter anything. \nPlease enter a value");
+                    reRun = true;
+                }
+                else
+                {
+                    break; // Break away from the while loop
+                }
+            }
+            return userInput.Trim().ToString();
         }
     }
 }
