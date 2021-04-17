20b86f87f6646af192b2d01136cdb8ba7e73632c
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-17 13:04:06 -0400

    pushing additional files

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static string GetWordsFromUser()
+        {
+            Console.Write("Please enter a word you would like to reverse: ");
+            string wordsFromUser = Console.ReadLine();
+            string stringCheck;
+            
+        }
+    }
+}
