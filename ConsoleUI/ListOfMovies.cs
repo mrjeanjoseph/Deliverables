89b477fc83453c5df4e0bc6a7bbfb0569a165262
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-20 07:52:51 -0400

    fixed the validation issues

--- ConsoleUI/ListOfMovies.cs
+++ ConsoleUI/ListOfMovies.cs
@@ -62,9 +62,9 @@ namespace ConsoleUI
             string formattedList = string.Format("{0, -15} {1, 5}", "Category", "Movie Title");
             string dashesAdded = string.Format("{0, -15} {1, 5}", "--------", "-----------");
             Console.WriteLine($"{ formattedList }\n{ dashesAdded }");
-            foreach (Movie movie in this._inventory)
+            foreach (Movie movies in this._inventory)
             {
-                formattedList = string.Format("{0, -15} {1, 5}", movie.Category, movie.Title);
+                formattedList = string.Format("{0, -15} {1, 5}", movies.Category, movies.Title);
                 Console.WriteLine(formattedList);
             }
         }
@@ -72,7 +72,7 @@ namespace ConsoleUI
         public string GetUserInput(string UserPrompt)
         {
             Console.WriteLine(UserPrompt);
-            return UserPrompt = Console.ReadLine(); // Validate this later (string only)
+            return UserPrompt = Console.ReadLine();
         }
 
         public Movie GetMoviesByCategory(string searching)
@@ -80,7 +80,7 @@ namespace ConsoleUI
             Movie category = null;
             foreach (Movie movies in this.Inventory)
             {
-                if (movies.Category == searching)
+                if (movies.Category.ToLower() == searching.ToLower())
                 {
                     category = movies;
                 }
@@ -88,20 +88,20 @@ namespace ConsoleUI
 
             if (category != null)
             {
+                Console.WriteLine($"Here's a list of movies from the { searching } category:");
                 foreach (Movie movies in this._inventory)
                 {
-                    if (searching == movies.Category)
+                    if (searching.ToLower() == movies.Category.ToLower())
                     {
-                        Console.WriteLine($" { movies.Title }"); //This should display a list of movies based on the category
+                        Console.WriteLine($" { movies.Title }"); //This has to display a list of movies based on the category
                     }
                 }
-                //Console.WriteLine($"{ catName }"); // I need a list of movies based on the category.
             }
             else
             {
-                Console.WriteLine($"No movies was found under { searching } category.");
+                Console.WriteLine($"No movies was found under { searching } category.\nPlease try again");
+                GetMoviesByCategory(GetUserInput(null));
             }
-
             return category;
         }
     }
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -7,14 +7,36 @@ namespace ConsoleUI
         static void Main(string[] args)
         {
             ListOfMovies viewAList = new ListOfMovies("Find Your Movies!");
-
             Console.WriteLine(viewAList.Greeting());
-            viewAList.DisplayAllMovies();
-            string userSearch = viewAList.GetUserInput("What category of movies are you interested in?: ");
 
-            Console.WriteLine($"Here's a list of movies from the { userSearch } category:");
-            viewAList.GetMoviesByCategory(userSearch);
+            bool reRunProgram = true;
+            while (reRunProgram)
+            {
+                viewAList.DisplayAllMovies();
+                string userSearch = viewAList.GetUserInput("What category of movies are you interested in?: ");
+                viewAList.GetMoviesByCategory(userSearch);
 
+                Console.WriteLine("Would you like to search again: (y/n)");
+                while (true)
+                {
+                    string tryAgain = Console.ReadLine();
+                    if (tryAgain == "y")
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else if (tryAgain == "n")
+                    {
+                        reRunProgram = false;
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please choose y/n");
+                    }
+                }
+            }
+            Console.WriteLine("Thank you for using our application");
             Console.ReadLine();
         }
     }
