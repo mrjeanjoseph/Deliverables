f677805ea573d304a686316d0af58c23ec55bfff
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-21 09:03:36 -0400

    Last update

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -13,9 +13,9 @@ namespace ConsoleUI
             Console.WriteLine("===========Extra Excercise2============");
             Console.WriteLine("Enter staff name, address, school and pay (Separate entries by comma)");
             //userInput = Console.ReadLine().Split(" ");
-            while (true)
+            while (true) // Validating user entries
             {
-                string[] userInput;
+                string[] userInput; // I had to use array.
                 try
                 {
                     userInput = Console.ReadLine().Split(", ");
@@ -25,7 +25,7 @@ namespace ConsoleUI
                 catch (Exception)
                 {
 
-                    Console.WriteLine("There was an error. Remember to Separate entries by commas.");
+                    Console.WriteLine("Invalid entries. Remember to Separate each entries by a comma.");
                 }
             }
             Console.ReadLine();
@@ -33,31 +33,36 @@ namespace ConsoleUI
 
         static void ListOfPersonsCreated()
         {
+            //Initializing a list of Person
             List<Person> coolPeople = new List<Person>()
             {
                 new Staff("Justin", "5511 staffing blvd", "Grand Circus", 350000),
                 new Staff("Kristen", "5511 staffing blvd", "Grand Circus", 340000),
-                new Student("Sean", "9957 student way", "C# .NET", 2021, 10000),
-                new Student("Kalai", "9957 student way", "C# .NET", 2021, 10000),
-                new Student("Jason", "9957 student way", "C# .NET", 2021, 10000)
+                new Student("Sean", "9957 student way", "C# DotNET", 2021, 10000),
+                new Student("Kalai", "9957 student way", "C# DotNET", 2021, 10000),
+                new Student("Alice", "9957 student way", "C# DotNET", 2021, 10000),
+                new Student("Jason", "9957 student way", "C# DotNET", 2021, 10000)
             };
 
-            Console.WriteLine($"A list of { coolPeople.Count } coolest people in the CG C#DotNet class.");
-            foreach (Person person in coolPeople)
+            Console.WriteLine($"A list of the { coolPeople.Count } coolest people in the CG C#DotNet class.");
+            foreach (Person person in coolPeople) // printing that list
             {
                 Console.WriteLine(person);
+                Console.WriteLine();
             }
         }
 
-        static void AddToPersonList(string[] userInput)// I could not find a way to split my list as user input the info
-        {
+        static void AddToPersonList(string[] userInput)
+        { // I could not find a way to split my List as user input the info
+          // I pass an array as parameter
+
             List<Person> personList = new List<Person>()
             {
-                new Staff(userInput[0], userInput[1], userInput[2], double.Parse(userInput[3]))
+                new Staff(userInput[0], userInput[1], userInput[2], double.Parse(userInput[3])) // I grabbed each one by index location
             };
             foreach (Person person in personList)
             {
-                Console.WriteLine(person);
+                Console.WriteLine(person); // printing the list.
             }
         }
     }
