8134925be32687f2eee7a8a044df3754299828fe
Jason <j_schubb53@yahoo.com>
2021-04-21 17:05:33 -0400

    added another change

--- UsedCarLot/Program.cs
+++ UsedCarLot/Program.cs
@@ -7,6 +7,9 @@ namespace UsedCarLot
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Change for GitHub branch");
+            Console.WriteLine("This is another change.");
         }
     }
 }
