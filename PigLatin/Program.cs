8fda779a199c546f58f3e02a1fc3057dad08d296
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-04-28 20:50:00 -0400

    bugs are everywhere

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace PigLatin
@@ -7,100 +8,13 @@ namespace PigLatin
     {
         static void Main(string[] args)
         {
+            string closeProgram = string.Empty;
             do
             {
                 string userInput = Translator.GetInput("Input a word or sentence to translate to pig Latin");
-
-                string translation = ToPigLatin(userInput);
-                Console.WriteLine(translation); 
+                Translator translator = new Translator(userInput);
+                translator.ToString();
             } while (true);
-        }
-
-        public static bool IsVowel(char c)
-        {
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-
-            return c.ToString() == vowels.ToString();
-        }
-
-        public static string CheckForSpecialChar(string word) // will use a short line of code for this one.
-        {
-            char[] specialChars = { '@', '.', '-', '$', '^', '&' };
-            word = word.ToLower();
-            foreach (char c in specialChars)
-            {
-                foreach (char w in word)
-                {
-                    if (w == c)
-                    {
-                        Console.WriteLine("That word has special characters, we will return it as is");
-                        word = word.Trim();
-                    }
-                }
-
-            }
-
-            return word;
-        }
-
-
-        public static string CheckForNoVowels(string word)
-        {
-            bool noVowels = true;
-            foreach (char letter in word)
-            {
-                if (IsVowel(letter))
-                {
-                    noVowels = false;
-                }
-            }
-
-            if (noVowels)
-            {
-                // the code reaches here and returned all words.
-                return word;
-            }
-            else
-            {
-                return "I reached here!"; // address this issue.                
-            }
-        }
-
-        public static string ToPigLatin(string word)
-        {
-            CheckForSpecialChar(word);
-
-            CheckForNoVowels(word);
-
-
-            // My breakpoint are not making it here
-            char firstLetter = word[0];
-            string output;
-            if (IsVowel(firstLetter) == false)
-            {
-                output = word + "ay";
-            }
-            else
-            {
-                int vowelIndex = -1;
-                string sub = word.Substring(vowelIndex + 1);
-                string postFix = word.Substring(0, vowelIndex - 1);
-
-                //Handle going through all the consonants
-                for (int i = 1; i <= word.Length; i++)
-                {
-                    if (IsVowel(word[i]) == true)
-                    {
-                        vowelIndex = i;
-                        break;
-                    }
-                }
-
-
-
-                output = sub + postFix + "ay";
-            }
-            return output;
         }
     }
 }
--- PigLatin/Translator.cs
+++ PigLatin/Translator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PigLatin
 {
@@ -15,6 +16,16 @@ namespace PigLatin
         public Translator(string words)
         {
             this._words = words;
+            ToPigLatin(words);
+        }
+
+        public Translator()
+        {
+            // default constructor
+        }
+        public override string ToString()
+        {
+            return $"{this._words}";
         }
 
         public static string GetInput(string prompt)
@@ -22,6 +33,79 @@ namespace PigLatin
             Console.WriteLine(prompt);
             string input = Console.ReadLine().ToLower().Trim();
             return input;
+        }
+
+        public static bool HasSpecialChars(string c)
+        {
+            if (c.Contains("@.-$^&!#%*)(_+=/+"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static string IsVowel(string vowels = "aeiou")
+        {
+            return vowels;
+        }
+
+        public static void ToPigLatin(string words)
+        {
+            HasSpecialChars(words);
+            List<string> output = new List<string>();
+
+            //Handle going through each words
+            foreach (string eachWord in words.Split(' '))
+            {
+                string firstLetter = eachWord.Substring(0, 1);
+                string postFix = eachWord.Substring(1, eachWord.Length - 1);
+
+                if (HasSpecialChars(words) == true)
+                {
+                    Console.WriteLine(words);
+                }
+                else if (IsVowel().IndexOf(firstLetter) == -1)
+                {
+                    output.Add($"{ postFix }{ firstLetter }ay");
+                }
+                else
+                {
+                    output.Add($"{ eachWord }way");
+                }
+            }
+            //return $" {output}"; Are you kidding me?
+            Console.WriteLine(string.Join(" ", output));
+            //return string.Join(" ", output);
+
+            // breakpoint not making it here
+            //char firstLetter = word[0];
+            //string output;
+            //if (vowels.Contains(firstLetter))
+            //{
+            //    output = word + "way";
+            //}
+            //else
+            //{
+            //    int vowelIndex = -1;
+
+            //    //Handle going through all the consonants
+            //    for (int i = 1; i <= word.Length; i++)
+            //    {
+            //        if (vowels.Contains(word[i]))
+            //        {
+            //            vowelIndex = i;
+            //            break;
+            //        }
+            //    }
+            //    string sub = word.Substring(vowelIndex + 1);
+            //    string postFix = word.Substring(0, vowelIndex - 1);
+
+            //    output = sub + postFix + "ay";
+            //}
+            //return output;
         }
 
     }
