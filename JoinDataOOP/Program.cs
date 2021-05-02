b54f529cf26b57e603af23407746c86cb4583b8c
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-02 10:55:21 -0400

    reworking the data table logic

--- JoinDataOOP/Program.cs
+++ JoinDataOOP/Program.cs
@@ -88,33 +88,59 @@ namespace JoiningDataOOP
 
             var distinctOrders = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
 
-            Console.WriteLine(string.Format("{0, -30}{1, -20} {2, -20} {3, -20} {4, 0}", "Customer", "Item", "Price", "Quantity", "Total"));
+
+            Console.WriteLine(string.Format("{0, -25}{1, -20}{2, -20}{3, -20}{4, 0}", "Customer", "Item", "Price", "Quantity", "Total"));
             foreach (Order name in distinctOrders)
             {
-
-                Console.Write(name.CustomerName);
-                string searching = name.CustomerName;
+                Console.Write(string.Format("{0, -25}", name.CustomerName));
                 for (int i = 0; i < orders.Count; i++)
                 {
                     if (name.CustomerName == orders[i].CustomerName)
                     {
-                        Console.WriteLine(string.Format("{0, -30} {1, -20} {2, -20} {3, -20} {4, 0}", null, orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
-                        //        string someVar = "";
-                        //            someVar = string.Format("{0, -35} {1, -20} {2, -20} {3, -20} {4, 0}", " ", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity);
-                        //        if (someVar.Contains(""))
-                        //        {
-                        //Console.WriteLine(string.Format("{0, -35} {1, -20} {2, -20} {3, -20} {4, 0}", "\t\t", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
-                        //        }
-                        //        else
-                        //        {
-                        //            Console.WriteLine(string.Format("{0, -30} {1, -20} {2, -20} {3, -20} {4, 0}", " ", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
-                        //        }
-
-
-                        //Console.WriteLine(orders[i].ToString());
+                        if (i == 1 || i == 4)
+                        {
+                            string addSpace = string.Format("{0, -25}", "\t\t\t");
+                            Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", addSpace, orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
+                        }
+                        //Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", $"{i}. ", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
                     }
                 }
             }
-        } // done
+        }
+
+
+        static void Exercise3Ext()
+        {
+            List<Order> orders = new List<Order>
+            {
+                new Order("Acme Hardware", "Mouse", 25, 3),
+                new Order("Acme Hardware", "Keyboard", 45, 2),
+                new Order("Falls Realty", "Macbook", 800, 2),
+                new Order("Julie’s Morning Diner", "iPad", 525, 1),
+                new Order("Julie’s Morning Diner", "Credit Card Reader", 45, 1),
+            };
+
+            var removeDuplicateNames = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
+
+            DataTableFormatter.PrintRow("Customer Name", "Item", "Price", "Quantity", "Total");
+            foreach (Order name in removeDuplicateNames)
+            {
+                //DataTableFormatter.PrintRow(name.CustomerName);
+                Console.Write(name.CustomerName);
+                for (int i = 0; i < orders.Count; i++)
+                {
+                    if (name.CustomerName == orders[i].CustomerName)
+                    {
+                        //foreach (Order order in removeDuplicateNames)
+                        //{
+                        DataTableFormatter.PrintRow(name.Item, name.Price.ToString(), name.Quantity.ToString(), (name.Price * name.Quantity).ToString());
+
+                        //}
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
     }
 }
