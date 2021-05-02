5f4938946fb5717d82f91e62cc2120316ef38433
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-02 11:07:37 -0400

    exercise 1, 2 & 3 completed

--- JoinDataOOP/Program.cs
+++ JoinDataOOP/Program.cs
@@ -40,7 +40,7 @@ namespace JoiningDataOOP
 
                 Console.WriteLine();
             }
-        } // done
+        } // Done
 
         static void Exercise2()
         {
@@ -73,7 +73,7 @@ namespace JoiningDataOOP
                 Console.WriteLine($"\tTotal \t\t\t\t\t{ total }");
                 Console.WriteLine();
             }
-        }
+        } // Done
 
         static void Exercise3()
         {
@@ -99,48 +99,17 @@ namespace JoiningDataOOP
                     {
                         if (i == 1 || i == 4)
                         {
-                            string addSpace = string.Format("{0, -25}", "\t\t\t");
+                            string addSpace = string.Format("{0, -25}", "");
                             Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", addSpace, orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
+                        }
+                        else
+                        {
+                            Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", "", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
                         }
                         //Console.WriteLine(string.Format("{0, 0}{1, -20}{2, -20}{3, -20}{4, 0}", $"{i}. ", orders[i].Item, orders[i].Price, orders[i].Quantity, orders[i].Price * orders[i].Quantity));
                     }
                 }
             }
-        }
-
-
-        static void Exercise3Ext()
-        {
-            List<Order> orders = new List<Order>
-            {
-                new Order("Acme Hardware", "Mouse", 25, 3),
-                new Order("Acme Hardware", "Keyboard", 45, 2),
-                new Order("Falls Realty", "Macbook", 800, 2),
-                new Order("Julie’s Morning Diner", "iPad", 525, 1),
-                new Order("Julie’s Morning Diner", "Credit Card Reader", 45, 1),
-            };
-
-            var removeDuplicateNames = orders.GroupBy(o => o.CustomerName).Select(o => o.First());
-
-            DataTableFormatter.PrintRow("Customer Name", "Item", "Price", "Quantity", "Total");
-            foreach (Order name in removeDuplicateNames)
-            {
-                //DataTableFormatter.PrintRow(name.CustomerName);
-                Console.Write(name.CustomerName);
-                for (int i = 0; i < orders.Count; i++)
-                {
-                    if (name.CustomerName == orders[i].CustomerName)
-                    {
-                        //foreach (Order order in removeDuplicateNames)
-                        //{
-                        DataTableFormatter.PrintRow(name.Item, name.Price.ToString(), name.Quantity.ToString(), (name.Price * name.Quantity).ToString());
-
-                        //}
-                    }
-                }
-            }
-            Console.ReadLine();
-
-        }
+        } // Done 
     }
 }
