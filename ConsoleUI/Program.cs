ce770bcb661eb98da110d988e9656db00ff3f20f
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-07 16:01:54 -0400

    added items to db

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,7 +6,32 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PopulateMovies();
+            Console.WriteLine("Saved");
+            Console.ReadLine();
+        }
+
+        static void PopulateMovies()
+        {
+            using (var context = new MovieDBContext())
+            {
+                Movie m1 = new Movie()
+                {
+                    Title = "Finding Neemo",
+                    Genre = "Animated",
+                    Runtime = 122.5
+                };
+                Movie m2 = new Movie()
+                {
+                    Title = "Too Fast",
+                    Genre = "Action",
+                    Runtime = 131.1
+                };
+
+                context.Movies.Add(m1);
+                context.Movies.Add(m2);
+                context.SaveChanges();
+            };
         }
     }
 }
