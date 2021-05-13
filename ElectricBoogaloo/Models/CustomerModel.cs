3eefa00e8e809a9c7b242df94e2e1a5c94a1a453
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-13 07:06:50 -0400

    first commit

---
+++ ElectricBoogaloo/Models/CustomerModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectricBoogaloo.Models
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+    }
+}
