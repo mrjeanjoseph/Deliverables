7fe11acd2d0c023a89d27752aa3e1043285d5679
Frednel Jean-Joseph <mr.jeanjoseph@gmail.com>
2021-05-13 21:55:10 -0400

    still working on the logic

--- DeckOfCards/Controllers/HomeController.cs
+++ DeckOfCards/Controllers/HomeController.cs
@@ -12,6 +12,7 @@ namespace DeckOfCards.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private GetDeckCardDAL _getDeckCardDAL = new GetDeckCardDAL();
 
         public HomeController(ILogger<HomeController> logger)
         {
@@ -20,12 +21,29 @@ namespace DeckOfCards.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //CardDeckModel cardDeckModel = getDeckCardDAL.ConvertJsonCardDeckModel();
+           return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult DrawCards()
+        {
+            CardDeckModel cardDeckModel = _getDeckCardDAL.ConvertJsonCardDeckModel();
+            //CardDeckModel cardDeckModel = new CardDeckModel();
+            //cardDeckModel = getDeckCardDAL.ConvertJsonCardDeckModel();
+            //return View(cardDeckModel);
+
+
+
+            CardDeckModel drawcard = _getDeckCardDAL.ConvertDrawCardsJson(cardDeckModel.deck_id, "5");
+
+            return View(drawcard);
+
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- DeckOfCards/Models/GetDeckCardDAL.cs
+++ DeckOfCards/Models/GetDeckCardDAL.cs
@@ -10,7 +10,7 @@ namespace DeckOfCards.Models
 {
     public class GetDeckCardDAL
     {
-        public string GetData() // the parameter allows them to use a city of choice
+        public string CreateNewDeck()
         {
             string url = @$"https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
 
@@ -25,14 +25,37 @@ namespace DeckOfCards.Models
             return JSON;
         }
 
-        public CardDeckModel ConvertJsonRedditModel()
+        public string DrawCardsJson(string drawFive, string deckId)
         {
-            string data = GetData();
+
+            string url = $@"https://deckofcardsapi.com/api/deck/{deckId}/draw/?count={drawFive}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            return JSON; // getting raw json
+        }
+
+        public CardDeckModel ConvertJsonCardDeckModel()
+        {
+            string data = CreateNewDeck();
 
             CardDeckModel cardDeckModel = JsonConvert.DeserializeObject<CardDeckModel>(data);
 
             return cardDeckModel;
         }
 
+        public CardDeckModel ConvertDrawCardsJson(string drawFive, string deckId)
+        {
+            string data = DrawCardsJson(drawFive, deckId);
+
+            CardDeckModel cardDraw = JsonConvert.DeserializeObject<CardDeckModel>(data);
+
+            return cardDraw;
+        }
     }
 }
