516ab82d7795942f635bf0ea4006781441d21ff3
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-10 06:43:14 -0400

    first commit - extending this repo

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -6,9 +6,7 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Learning GitHub Branching and merging");
-            Console.WriteLine("I am making changes over here");
-            Console.WriteLine("======================================");
+            Console.WriteLine("Hello World!");
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# RepoA
+# Explorable Maze
+
+This projcet is an expansion from Michael Hadley's [explorable maze](https://www.youtube.com/playlist?list=PL-LDQE9x9hLwldZPPGwqXixr-_DfINfxk). 
