e879804f8a30589c2b21987a011bcf6bf9ee4d88
mrjeanjoseph <mr.jeanjoseph@gmail.com>
2021-07-10 06:49:52 -0400

    cooking up the skeleton of the app

---
+++ ConsoleUI/Model/GameModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Model
+{
+    class GameModel
+    {
+        public void Start()
+        {
+
+        }
+    }
+}
